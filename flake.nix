4004581c97d65cdbcd86de5fb6cbaadda12d2dd2
Kodlak15 <stanlcod15@gmail.com>
2024-06-01 14:19:55 -0700

    Upgrade nixpkgs-stable to 24.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     flake-utils = {
       url = "github:numtide/flake-utils";
