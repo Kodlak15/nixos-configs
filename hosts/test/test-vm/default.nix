4deb12c9acdc55bd5418dfb1bc3f43573cf9c6d8
Kodlak15 <stanlcod15@gmail.com>
2024-06-21 00:01:20 -0700

    Trying out manually managing filesystems

--- hosts/test/test-vm/default.nix
+++ hosts/test/test-vm/default.nix
@@ -5,12 +5,42 @@
 }: let
   username = "user";
   version = "24.05";
+  # luks = "nixos-crypt";
+  # luksPart = "/dev/disk/by-label/NIXOS";
+  rootPart = "/dev/disk/by-label/ROOT";
 in {
   imports = [
     ./hardware-configuration.nix
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # boot.initrd.luks.devices.${luks}.device = luksPart;
+
+  fileSystems = {
+    "/" = {
+      device = rootPart;
+      fsType = "btrfs";
+      options = ["subvol=@"];
+    };
+    "/home" = {
+      device = rootPart;
+      fsType = "btrfs";
+      options = ["subvol=@home"];
+    };
+    "/tmp" = {
+      device = rootPart;
+      fsType = "btrfs";
+      options = ["subvol=@tmp"];
+    };
+    "/var" = {
+      device = rootPart;
+      fsType = "btrfs";
+      options = ["subvol=@var"];
+    };
+  };
+
+  swapDevices = [];
 
   boot = {
     loader.systemd-boot.enable = true;
