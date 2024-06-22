d47ddf1736e2fa15bf9167576df0c17e83994b75
Kodlak15 <stanlcod15@gmail.com>
2024-06-21 22:40:25 -0700

    Update powder module

--- home/cody/everest/powder/default.nix
+++ home/cody/everest/powder/default.nix
@@ -1,34 +1,39 @@
 {
   powder = {
     enable = true;
+    # TODO this should be a list of attribute sets
     systems = {
       default = true;
-      flake = /home/cody/nix/flakes/nixos;
+      flake = {
+        path = "/home/cody/nix/flakes/nixos";
+        url = "github:Kodlak15/nixos-flake";
+      };
       nixos.name = "personal/everest";
-      hm.user = "testtesttest";
-      install = {
-        disks = {
-          encryption = {
+      home.user = "cody";
+      disks = {
+        encryption = {
+          enable = true;
+          label = "NIXOS";
+          mapping = "nixos-crypt";
+          smartcard = {
             enable = true;
-            label = "NIXOS-CRYPT";
-            smartcard = {
-              enable = true;
-              slot = 2;
-            };
+            slot = 2;
+            saltLength = 16;
+            cipher = "aes-xts-plain64";
+            hash = "sha512";
           };
-          root = {
-            label = "NIXOS";
-            size = null; # use rest of disk
-            filesystem = "btrfs";
-          };
-          boot = {
-            label = "EFI-NIXOS";
-            size = 512;
-          };
+        };
+        root = {
+          label = "NIXOS";
+          size = null; # use rest of disk
+          filesystem = "btrfs";
+        };
+        boot = {
+          label = "EFI-NIXOS";
+          size = 512;
         };
       };
       jobs = 8;
     };
-    # config = ./config.yaml;
   };
 }
--- modules/home-manager/powder/default.nix
+++ modules/home-manager/powder/default.nix
@@ -23,62 +23,90 @@ in {
         type = lib.types.bool;
         default = false;
       };
-      flake = lib.mkOption {
-        type = lib.types.path;
-        default = /etc/nixos;
+      flake = {
+        path = lib.mkOption {
+          type = lib.types.string;
+          default = "";
+        };
+        url = lib.mkOption {
+          type = lib.types.string;
+          default = "";
+        };
       };
       nixos.name = lib.mkOption {
         type = lib.types.string;
         default = "nixos";
       };
-      hm.user = lib.mkOption {
+      home.user = lib.mkOption {
         type = lib.types.string;
         default = "user";
       };
-      install = {
-        disks = {
-          encryption = {
+      disks = {
+        encryption = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+          };
+          label = lib.mkOption {
+            type = lib.types.string;
+            default = "NIXOS";
+          };
+          mapping = lib.mkOption {
+            type = lib.types.string;
+            default = "nixos-crypt";
+          };
+          smartcard = {
             enable = lib.mkOption {
               type = lib.types.bool;
               default = false;
             };
-            label = lib.mkOption {
-              type = lib.types.string;
-              default = "NIXOS-CRYPT";
+            slot = lib.mkOption {
+              type = lib.types.ints.between 1 2;
             };
-            smartcard = {
-              enable = lib.mkOption {
-                type = lib.types.bool;
-                default = false;
-              };
-              slot = lib.mkOption {
-                type = lib.types.ints.between 1 2;
-              };
+            saltLength = lib.mkOption {
+              type = lib.types.ints.between 16 64;
+              default = 16;
             };
-          };
-          root = {
-            label = lib.mkOption {
-              type = lib.types.string;
-              default = "NIXOS";
-            };
-            size = lib.mkOption {
-              type = lib.types.nullOr lib.types.int;
-              default = null;
-            };
-            filesystem = lib.mkOption {
-              type = lib.types.string;
-              default = "btrfs";
-            };
-          };
-          boot = {
-            label = lib.mkOption {
-              type = lib.types.string;
-              default = "EFI-NIXOS";
-            };
-            size = lib.mkOption {
+            keyLength = lib.mkOption {
               type = lib.types.int;
               default = 512;
             };
+            iterations = lib.mkOption {
+              type = lib.types.int;
+              default = 1000000;
+            };
+            cipher = lib.mkOption {
+              type = lib.types.string;
+              default = "aes-xts-plain64";
+            };
+            hash = lib.mkOption {
+              type = lib.types.string;
+              default = "sha512";
+            };
+          };
+        };
+        root = {
+          label = lib.mkOption {
+            type = lib.types.string;
+            default = "NIXOS";
+          };
+          size = lib.mkOption {
+            type = lib.types.nullOr lib.types.int;
+            default = null;
+          };
+          filesystem = lib.mkOption {
+            type = lib.types.string;
+            default = "btrfs";
+          };
+        };
+        boot = {
+          label = lib.mkOption {
+            type = lib.types.string;
+            default = "EFI-NIXOS";
+          };
+          size = lib.mkOption {
+            type = lib.types.int;
+            default = 512;
           };
         };
       };
@@ -95,27 +123,34 @@ in {
       "powder/config.yaml".text = ''
         example:
           default: ${boolConvert systems.default}
-          flake: ${systems.flake}
+          flake:
+            path: ${systems.flake.path}
+            url: ${systems.flake.url}
           nixos:
             name: ${systems.nixos.name}
-          hm:
-            user: ${systems.hm.user}
-        install:
+          home:
+            user: ${systems.home.user}
           disks:
             encryption:
-              enable: ${boolConvert systems.install.disks.encryption.enable}
-              label: ${systems.install.disks.encryption.label}
+              enable: ${boolConvert systems.disks.encryption.enable}
+              label: ${systems.disks.encryption.label}
+              mapping: ${systems.disks.encryption.mapping}
               smartcard:
-                enable: ${boolConvert systems.install.disks.encryption.smartcard.enable}
-                slot: ${builtins.toString systems.install.disks.encryption.smartcard.slot}
+                enable: ${boolConvert systems.disks.encryption.smartcard.enable}
+                slot: ${builtins.toString systems.disks.encryption.smartcard.slot}
+                salt_length: ${builtins.toString (systems.disks.encryption.smartcard.saltLength)}
+                key_length: ${builtins.toString systems.disks.encryption.smartcard.keyLength}
+                iterations: ${builtins.toString systems.disks.encryption.smartcard.iterations}
+                cipher: ${systems.disks.encryption.smartcard.cipher}
+                hash: ${systems.disks.encryption.smartcard.hash}
             root:
-              label: ${systems.install.disks.root.label}
-              size: ${builtins.toString (rootSize systems.install.disks.root.size)}
-              filesystem: ${systems.install.disks.root.filesystem}
+              label: ${systems.disks.root.label}
+              size: ${builtins.toString (rootSize systems.disks.root.size)}
+              filesystem: ${systems.disks.root.filesystem}
             boot:
-              label: ${systems.install.disks.boot.label}
-              size: ${builtins.toString systems.install.disks.boot.size}
-        jobs: ${builtins.toString systems.jobs}
+              label: ${systems.disks.boot.label}
+              size: ${builtins.toString systems.disks.boot.size}
+          jobs: ${builtins.toString systems.jobs}
       '';
     };
 
