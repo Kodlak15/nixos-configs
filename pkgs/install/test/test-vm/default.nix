15747fa39fced92158054f843be80f8b0d3a62a1
Kodlak15 <stanlcod15@gmail.com>
2024-06-25 01:44:06 -0700

    Testing out build in install script

--- pkgs/install/test/test-vm/default.nix
+++ pkgs/install/test/test-vm/default.nix
@@ -7,7 +7,7 @@
     pkgs.writeShellScriptBin
     "hextorb"
     ''( tr '[:lower:]' '[:upper:]' | sed -e 's/\([0-9A-F]\{2\}\)/\\\\\\x\1/gI'| xargs printf )'';
-  pbkdf2Sha512 = pkgs.callPackage ./nix/pbkdf2-sha512.nix {};
+  pbkdf2Sha512 = pkgs.callPackage ../../pbkdf2-sha512.nix {};
 in
   pkgs.writeShellScriptBin "install.sh" ''
     set -e
