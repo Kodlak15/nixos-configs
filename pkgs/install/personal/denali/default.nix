beda3d82cf15d21e8360ef1096d97015f50faf88
Kodlak15 <stanlcod15@gmail.com>
2024-06-25 22:28:52 -0700

    Add install script for personal configs

--- pkgs/install/personal/denali/default.nix
+++ pkgs/install/personal/denali/default.nix
@@ -1,117 +1,151 @@
-{pkgs, ...}:
-pkgs.writeShellScriptBin "install.sh" ''
-  set -e
+{pkgs, ...}: let
+  helpers = import ../../helpers.nix {inherit pkgs;};
+  inherit (helpers) rbtohex hextorb pbkdf2Sha512;
+in
+  pkgs.writeShellScriptBin "install.sh" ''
+    set -e
 
-  # Make sure user running the script is root
-  if [[ ! "$EUID" -eq 0 ]]; then
-  	echo "This must be run as root!"
-  	echo "Run 'sudo -i' to switch to the root user."
-  	exit 1
-  fi
+    # Make sure user running the script is root
+    if [[ ! "$EUID" -eq 0 ]]; then
+    	echo "This must be run as root!"
+    	echo "Run 'sudo -i' to switch to the root user."
+    	exit 1
+    fi
 
-  # The flake to use
-  FLAKE="github:Kodlak15/nixos-flake"
+    # The flake to use
+    FLAKE="github:Kodlak15/nixos-flake"
 
-  # Select disks to use
-  read -p "Choose the disk to use for LUKS: " "LUKSDISK"
-  read -p "Choose the disk to use for boot: " "BOOTDISK"
+    # Select disks to use
+    lsblk
+    read -p "Choose the disk to use for LUKS: " LUKSDISK
+    read -p "Choose the disk to use for BOOT: " BOOTDISK
+    LUKSDISK="/dev/$LUKSDISK"
+    BOOTDISK="/dev/$BOOTDISK"
 
-  # Disk to be used and its partitions
-  LUKSDISK="/dev/vda"
-  BOOTPART="/dev/vda1"
-  ROOTPART="/dev/vda2"
+    read -p "Choose the partition number for LUKS: " NLUKS
+    read -p "Choose the partition number for BOOT: " NBOOT
 
-  # The size of the boot partition (mb)
-  BOOTSIZE=512
+    if [[ "$LUKSDISK" == "$BOOTDISK" && "$NLUKS" == "$NBOOT" ]]; then
+      echo "Error: attempting to use the same partition for LUKS and BOOT, exiting..."
+      exit 1
+    fi
 
-  # Mountpoint used during installation
-  MOUNTPOINT="/mnt/nixos"
+    if [[ -n "$(echo "$LUKSDISK" | grep "nvme")" ]]; then
+      ENDLUKS="p$NLUKS"
+    else
+      ENDLUKS="$NLUKS"
+    fi
 
-  # The NixOS configuration to use
-  NIXCFG="test/test-vm"
+    if [[ -n "$(echo "$BOOTDISK" | grep "nvme")" ]]; then
+      ENDBOOT="p$NBOOT"
+    else
+      ENDBOOT="$NBOOT"
+    fi
 
-  # Slot to be used on the yubikey (set to 0 to skip programming)
-  SLOT="2"
+    LUKSPART="$LUKSDISK$ENDLUKS"
+    BOOTPART="$BOOTDISK$ENDBOOT"
 
-  # Program the yubikey
-  if [[ ! "$SLOT" == "0" ]]; then
-  	ykpersonalize -"$SLOT" -ochal-resp -ochal-hmac
-  fi
+    # The size of the boot partition (mb)
+    BOOTSIZE=512
 
-  # Create the partitions
-  sgdisk -g "$LUKSDISK"
-  sgdisk -n 1::+"$BOOTSIZE"M --typecode=1:ef00 "$LUKSDISK"
-  sgdisk -n 2::-0 --typecode=2:8300 "$LUKSDISK"
-  partprobe "$LUKSDISK"
+    # Mountpoint used during installation
+    MOUNTPOINT="/mnt/nixos"
 
-  # Read 2FA password
-  echo "Enter 2FA password:"
-  read -s USER_PASSPHRASE
+    # The NixOS configuration to use
+    NIXCFG="test/test-vm"
 
-  # Calculate the initial salt (length can be any integer in range 16-64)
-  SALT_LENGTH=16
-  SALT="$(dd if=/dev/random bs=1 count=$SALT_LENGTH 2>/dev/null | rbtohex)"
+    # Slot to be used on the yubikey (set to 0 to skip programming)
+    SLOT="2"
 
-  # Calculate the initial challenge and response
-  CHALLENGE="$(echo -n $SALT | openssl dgst -binary -sha512 | rbtohex)"
-  RESPONSE=$(ykchalresp -2 -x $CHALLENGE 2>/dev/null)
+    # Program the yubikey
+    if [[ ! "$SLOT" == "0" ]]; then
+    	ykpersonalize -"$SLOT" -y -ochal-resp -ochal-hmac &>/dev/null
+    fi
 
-  # Calculate the LUKS slot key
-  KEY_LENGTH=512
-  ITERATIONS=1000000
-  LUKS_KEY="$(echo -n $USER_PASSPHRASE | pbkdf2-sha512 $(($KEY_LENGTH / 8)) $ITERATIONS $RESPONSE | rbtohex)"
+    # Wipe and format the LUKS disk
+    echo "$LUKSDISK will now be formatted and all data will be wiped!"
+    read -p "Continue? (y/n): " CONFIRM
+    if [[ "$CONFIRM" != "y" ]]; then
+      echo "Exiting..."
+      exit 0
+    else
+      sgdisk -o "$LUKSDISK" &>/dev/null
+      sgdisk -g "$LUKSDISK" &>/dev/null
+    fi
 
-  # Create the LUKS device
-  CIPHER=aes-xts-plain64
-  HASH=sha512
-  echo -n "$LUKS_KEY" | hextorb | cryptsetup luksFormat --label "NIXOS" --cipher="$CIPHER" --key-size="$KEY_LENGTH" --hash="$HASH" --key-file=- "$ROOTPART"
+    # Create the partitions
+    sgdisk -n "$NBOOT"::+"$BOOTSIZE"M --typecode=1:ef00 "$LUKSDISK" &>/dev/null
+    sgdisk -n "$NLUKS"::-0 --typecode=2:8300 "$LUKSDISK" &>/dev/null
+    partprobe "$LUKSDISK" &>/dev/null
 
-  # Create the boot filesystem
-  mkfs.fat -F 32 -n "EFI-NIXOS" "$BOOTPART"
+    # Read 2FA password
+    echo "Enter 2FA password:"
+    read -s USER_PASSPHRASE
 
-  # Store the salt and iterations on the boot volume
-  mount --mkdir "$BOOTPART" /boot
-  mkdir -p /boot/crypt-storage
-  echo -ne "$SALT\n$ITERATIONS" > /boot/crypt-storage/default
-  umount /boot
+    # Calculate the initial salt (length can be any integer in range 16-64)
+    SALT_LENGTH=16
+    SALT="$(dd if=/dev/random bs=1 count=$SALT_LENGTH 2>/dev/null | "${rbtohex}/bin/rbtohex")"
 
-  # Open the LUKS device
-  echo -n "$LUKS_KEY" | hextorb | cryptsetup open "$ROOTPART" nixos-crypt --key-file=-
+    # Calculate the initial challenge and response
+    CHALLENGE="$(echo -n $SALT | openssl dgst -binary -sha512 | "${rbtohex}/bin/rbtohex")"
+    RESPONSE=$(ykchalresp -2 -x $CHALLENGE 2>/dev/null)
 
-  # The encrypted root partition of the new system
-  ROOT="/dev/mapper/nixos-crypt"
+    # Calculate the LUKS slot key
+    KEY_LENGTH=512
+    ITERATIONS=1000000
+    LUKS_KEY="$(echo -n $USER_PASSPHRASE | ${pbkdf2Sha512}/bin/pbkdf2-sha512 $(($KEY_LENGTH / 8)) $ITERATIONS $RESPONSE | ${rbtohex}/bin/rbtohex)"
 
-  # Create the root filesytem
-  mkfs.btrfs -L "ROOT" "$ROOT" -f
+    # Create the LUKS device
+    CIPHER=aes-xts-plain64
+    HASH=sha512
+    echo -n "$LUKS_KEY" | "${hextorb}/bin/hextorb" | cryptsetup luksFormat --label "NIXOS" --cipher="$CIPHER" --key-size="$KEY_LENGTH" --hash="$HASH" --key-file=- "$LUKSPART" &>/dev/null
 
-  # Create subvolumes
-  mount --mkdir "$ROOT" "$MOUNTPOINT"
-  btrfs subvolume create "$MOUNTPOINT/@"
-  btrfs subvolume create "$MOUNTPOINT/@home"
-  btrfs subvolume create "$MOUNTPOINT/@tmp"
-  btrfs subvolume create "$MOUNTPOINT/@var"
-  umount "$MOUNTPOINT"
+    # Create the boot filesystem
+    mkfs.fat -F 32 -n "EFI-NIXOS" "$BOOTPART" &>/dev/null
 
-  # Mount the subvolumes
-  mount -o subvol="@" "$ROOT" "$MOUNTPOINT"
-  mount --mkdir -o subvol="@home" "$ROOT" "$MOUNTPOINT/home"
-  mount --mkdir -o subvol="@tmp" "$ROOT" "$MOUNTPOINT/tmp"
-  mount --mkdir -o subvol="@var" "$ROOT" "$MOUNTPOINT/var"
+    # Store the salt and iterations on the boot volume
+    mount --mkdir "$BOOTPART" /boot
+    mkdir -p /boot/crypt-storage
+    echo -ne "$SALT\n$ITERATIONS" > /boot/crypt-storage/default
+    umount /boot
 
-  # Mount the boot partition
-  mount -o umask=0077 --mkdir "$BOOTPART" "$MOUNTPOINT/boot"
+    # Open the LUKS device
+    echo -n "$LUKS_KEY" | "${hextorb}/bin/hextorb" | cryptsetup open "$LUKSPART" nixos-crypt --key-file=-
 
-  # Generate the initial configuration
-  nixos-generate-config --root "$MOUNTPOINT" --no-filesystems
+    # The encrypted root partition of the new system
+    ROOT="/dev/mapper/nixos-crypt"
 
-  # Install the system
-  nixos-install --root "$MOUNTPOINT" --flake "$FLAKE#$NIXCFG"
+    # Create the root filesytem
+    mkfs.btrfs -L "ROOT" "$ROOT" -f &>/dev/null
 
-  # Unmount all volumes
-  umount -R "$MOUNTPOINT"
+    # Create subvolumes
+    mount --mkdir "$ROOT" "$MOUNTPOINT"
+    btrfs subvolume create "$MOUNTPOINT/@" &>/dev/null
+    btrfs subvolume create "$MOUNTPOINT/@home" &>/dev/null
+    btrfs subvolume create "$MOUNTPOINT/@tmp" &>/dev/null
+    btrfs subvolume create "$MOUNTPOINT/@var" &>/dev/null
+    umount "$MOUNTPOINT"
 
-  # Close the LUKS partition
-  cryptsetup luksClose "nixos-crypt"
+    # Mount the subvolumes
+    mount -o subvol="@" "$ROOT" "$MOUNTPOINT"
+    mount --mkdir -o subvol="@home" "$ROOT" "$MOUNTPOINT/home"
+    mount --mkdir -o subvol="@tmp" "$ROOT" "$MOUNTPOINT/tmp"
+    mount --mkdir -o subvol="@var" "$ROOT" "$MOUNTPOINT/var"
 
-  echo "NixOS was installed successfully!"
-''
+    # Mount the boot partition
+    mount -o umask=0077 --mkdir "$BOOTPART" "$MOUNTPOINT/boot"
+
+    # Generate the initial configuration
+    nixos-generate-config --root "$MOUNTPOINT" --no-filesystems &>/dev/null
+
+    # Install the system (skip setting initial password)
+    nixos-install --root "$MOUNTPOINT" --flake "$FLAKE#$NIXCFG" --no-root-password &>/dev/null
+
+    # Unmount all volumes
+    umount -R "$MOUNTPOINT"
+
+    # Close the LUKS partition
+    cryptsetup luksClose "nixos-crypt"
+
+    echo "NixOS was installed successfully!"
+  ''
