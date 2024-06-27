ea81b6a8bc79cd51fd5a792d108c8a7ef310811c
Kodlak15 <stanlcod15@gmail.com>
2024-06-27 12:55:48 -0700

    Old stuffs

--- home/cody/denali/hyprland/windowSettings.nix
+++ home/cody/denali/hyprland/windowSettings.nix
@@ -29,9 +29,9 @@
           preserve_split = true # you probably want this
       }
 
-      master {
-          new_is_master = true
-      }
+      # master {
+      #     new_is_master = true
+      # }
     '';
   };
 }
