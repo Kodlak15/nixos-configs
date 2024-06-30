71dd27157bf54bd2b812eb1bb9f7627bb845b86f
Kodlak15 <stanlcod15@gmail.com>
2024-06-30 14:36:06 -0700

    Try xfce in vm

--- hosts/vm/korriban/default.nix
+++ hosts/vm/korriban/default.nix
@@ -136,10 +136,23 @@ in {
         KbdInteractiveAuthentication = false;
       };
     };
+    # xserver = {
+    #   enable = true;
+    #   displayManager.sddm.enable = true;
+    #   desktopManager.plasma6.enable = true;
+    # };
     xserver = {
       enable = true;
-      displayManager.sddm.enable = true;
-      desktopManager.plasma6.enable = true;
+      desktopManager = {
+        xterm.enable = false;
+        xfce = {
+          enable = true;
+          noDesktop = true;
+          enableXfwm = false;
+        };
+      };
+      displayManager.defaultSession = "xfce";
+      windowManager.i3.enable = true;
     };
   };
 
