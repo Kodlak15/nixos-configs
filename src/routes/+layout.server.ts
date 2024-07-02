d509f6f6d5448a87361e5681750a9c99e6ab20ca
Kodlak15 <stanlcod15@gmail.com>
2024-07-01 22:03:08 -0700

    Debugging session issue

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,6 +4,7 @@ import { getUser } from "$lib/utils/user";
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	const token = cookies.get("session");
+	console.log("token:", token);
 
 	if (token) {
 		const result = await pool.query("\
@@ -12,6 +13,7 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 			WHERE token = $1 AND expires_at < NOW()", [token]);
 
 		const rows = result.rows;
+		console.log("rows:", rows);
 		if (rows.length === 1) {
 			const user = await getUser(rows[0].uid);
 			return {
