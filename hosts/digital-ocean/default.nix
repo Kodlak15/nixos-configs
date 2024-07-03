d81eddb5a22c919f0bacd6fd1ccfb38fe4ffd725
Kodlak15 <stanlcod15@gmail.com>
2024-07-03 02:05:14 -0700

    Add stuff to digital ocean image

--- hosts/digital-ocean/default.nix
+++ hosts/digital-ocean/default.nix
@@ -1,8 +1,20 @@
-{pkgs, ...}: {
-  # TODO
-  # ...
-  # ...
-  # ...
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    "${inputs.nixpkgs}/nixos/modules/virtualisation/digital-ocean-image.nix"
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  virtualisation.digitalOceanImage.compressionMethod = "bzip2";
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    neofetch
+  ];
 
   system.stateVersion = "24.11";
 }
