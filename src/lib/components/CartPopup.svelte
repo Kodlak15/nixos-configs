296021314dd3edeee983072d2e2952f7ae2063dd
Kodlak15 <stanlcod15@gmail.com>
2024-07-23 14:23:30 -0700

    Add checkout page

--- src/lib/components/CartPopup.svelte
+++ src/lib/components/CartPopup.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import CartPopupItem from "$lib/components/CartPopupItem.svelte";
+	import { toggleCartPopup } from "$lib/ui";
 	import { cart, cartSubtotal, numItemsInCart } from "$lib/stores";
 </script>
 
 <!-- TODO this could look better -->
 <div
-	class="hidden bg-white absolute top-full right-6 mt-2 p-6 border-black border-2 border-solid rounded-md
+	class="bg-white absolute top-full right-6 mt-2 p-6 border-black border-2 border-solid rounded-md
 				z-[99] overflow-auto h-96 w-[22rem]"
 	id="cart-popup"
 >
@@ -29,6 +30,13 @@
 					<CartPopupItem {item} />
 				{/if}
 			{/each}
+			<div class="flex justify-start items-center">
+				<button on:click={toggleCartPopup}>
+					<a href="/checkout" class="text-white text-2xl text-center">
+						<div class="bg-feldgrau p-2 rounded-md">Checkout</div>
+					</a>
+				</button>
+			</div>
 		{/if}
 	</div>
 </div>
---
+++ src/routes/checkout/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col justify-center items-center gap-2 p-6">
+	<h1 class="text-feldgrau text-2xl font-bold">Under Construction</h1>
+	<p>Checkout</p>
+</div>
