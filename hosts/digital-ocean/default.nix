9814f5d673849befc9a8b781f4e2a371c076d9c6
Kodlak15 <stanlcod15@gmail.com>
2024-07-23 21:20:34 -0700

    Work on nginx config for do image

--- hosts/digital-ocean/default.nix
+++ hosts/digital-ocean/default.nix
@@ -16,6 +16,22 @@
     neofetch
   ];
 
+  # TODO add option to flake to rebuild droplet so I don't need to rebuild this each time
+  services = {
+    nginx = {
+      enable = true;
+      virtualHosts."myhost.org" = {
+        addSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:3000";
+          };
+        };
+      };
+    };
+  };
+
   users.users = {
     root = {
       openssh.authorizedKeys.keys = [
