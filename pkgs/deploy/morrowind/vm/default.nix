920a826eb89c3fb5875f539d752378e93e9401d0
Kodlak15 <stanlcod15@gmail.com>
2024-08-05 21:49:48 -0700

    This is still a huge mess

--- pkgs/deploy/morrowind/vm/default.nix
+++ pkgs/deploy/morrowind/vm/default.nix
@@ -1,4 +1,4 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "deploy" ''
-  nixos-rebuild switch --flake .#morrowind/vm --target-host morrowind-laptop-vm --build-host cody@localhost --max-jobs auto
+  nixos-rebuild switch --flake .#morrowind/vm --target-host morrowind-vm --build-host cody@localhost --max-jobs auto
 ''
