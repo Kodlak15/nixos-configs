51bae20cbe1ad8e116ebe5e74c8dacf4069446e6
Kodlak15 <stanlcod15@gmail.com>
2024-08-05 22:24:09 -0700

    This is still a huge mess

--- hosts/digital-ocean/morrowind/postgres/default.nix
+++ hosts/digital-ocean/morrowind/postgres/default.nix
@@ -55,9 +55,10 @@ in {
       authentication = pkgs.lib.mkOverride 10 ''
         #type database  DBuser  auth-method
         local all       all     trust
-        host  all       all     192.168.122.1/32 trust
-        host  all       all     172.17.0.0/16    trust
-        host  all       all     ::1/128          trust
+        # host  all       all     192.168.122.1/32 trust
+        host  all       all     192.168.122.142/24 trust
+        host  all       all     172.17.0.0/16      trust
+        host  all       all     ::1/128            trust
       '';
       enableTCPIP = true;
     };
