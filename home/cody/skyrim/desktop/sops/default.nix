0bbb51c9710ef377657a4aff4c8d0d996b57bc1e
Kodlak15 <stanlcod15@gmail.com>
2024-08-13 14:12:36 -0700

    Switching around sops stuff

--- home/cody/skyrim/desktop/sops/default.nix
+++ home/cody/skyrim/desktop/sops/default.nix
@@ -7,23 +7,27 @@
     age = {
       keyFile = "/home/cody/.config/sops/age/keys.txt";
     };
-    defaultSopsFile = ./secrets.yaml;
     validateSopsFiles = false;
     secrets = {
       "private_keys/sops" = {
         path = "/home/cody/.config/sops/age/keys.txt";
+        sopsFile = ./secrets.yaml;
       };
       "private_keys/pwncollege" = {
         path = "/home/cody/.ssh/pwn";
+        sopsFile = ./secrets.yaml;
       };
       "private_keys/digitalOcean" = {
         path = "/home/cody/.ssh/digitalOcean";
+        sopsFile = ./secrets.yaml;
       };
       "private_keys/github" = {
         path = "/home/cody/.ssh/gh";
+        sopsFile = ./secrets.yaml;
       };
       "private_keys/cyrodil" = {
         path = "/home/cody/.ssh/cyrodil";
+        sopsFile = ./secrets.yaml;
       };
     };
   };
