75240ffb8b673b1e62338a5ec6f14bd8d100ee7a
Kodlak15 <stanlcod15@protonmail.com>
2024-10-05 12:57:29 -0700

    Got sops user password working in VM

--- hosts/rift/default.nix
+++ hosts/rift/default.nix
@@ -21,7 +21,7 @@
 
   sops = {
     age = {
-      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      sshKeyPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
@@ -30,6 +30,41 @@
         sopsFile = ./secrets.yaml;
         neededForUsers = true;
       };
+    };
+  };
+
+  users = {
+    mutableUsers = false;
+    users = {
+      cody = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        ignoreShellProgramCheck = true;
+        hashedPasswordFile = config.sops.secrets.password.path;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0jIg6UYuO+MSjBEcaaJXAoY3yLl7q7tqMVB0yFiqGr"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3fdO9C72OaYhK3W2OhINnlRCcopOblJJI/z9frc1F0"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwaOrqTJ6Xq8qU3y/Vn02tHMUZJISNRA/fLAVfYCN21"
+        ];
+        group = "cody";
+        extraGroups = [
+          "wheel"
+          "docker"
+          "wireshark"
+        ];
+      };
+      root = {
+        initialHashedPassword = "$y$j9T$vVNMHVnnesyBcnbDxNQ7T/$JqywYFyoePyzWLEW822Sv2pWYyQoLJgAo.wCzurMyNC";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0jIg6UYuO+MSjBEcaaJXAoY3yLl7q7tqMVB0yFiqGr"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3fdO9C72OaYhK3W2OhINnlRCcopOblJJI/z9frc1F0"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwaOrqTJ6Xq8qU3y/Vn02tHMUZJISNRA/fLAVfYCN21"
+        ];
+      };
+    };
+    # TODO:
+    groups = {
+      cody = {};
     };
   };
 
@@ -47,35 +82,35 @@
       systemd = {
         enable = true;
         services = {
-          # wipe-root = {
-          #   requires = ["dev-mapper-root.device"];
-          #   after = ["dev-mapper-root.device"];
-          #   wantedBy = ["initrd.target"];
-          #   script = lib.mkAfter ''
-          #     mkdir /btrfs_tmp
-          #     mount /dev/mapper/root /btrfs_tmp
-          #     if [[ -e /btrfs_tmp/root ]]; then
-          #         mkdir -p /btrfs_tmp/old_roots
-          #         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          #         mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-          #     fi
-          #
-          #     delete_subvolume_recursively() {
-          #         IFS=$'\n'
-          #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-          #             delete_subvolume_recursively "/btrfs_tmp/$i"
-          #         done
-          #         btrfs subvolume delete "$1"
-          #     }
-          #
-          #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          #         delete_subvolume_recursively "$i"
-          #     done
-          #
-          #     btrfs subvolume create /btrfs_tmp/root
-          #     umount /btrfs_tmp
-          #   '';
-          # };
+          wipe-root = {
+            requires = ["dev-mapper-root.device"];
+            after = ["dev-mapper-root.device"];
+            wantedBy = ["initrd.target"];
+            script = lib.mkAfter ''
+              mkdir /btrfs_tmp
+              mount /dev/mapper/root /btrfs_tmp
+              if [[ -e /btrfs_tmp/root ]]; then
+                  mkdir -p /btrfs_tmp/old_roots
+                  timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+                  mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+              fi
+
+              delete_subvolume_recursively() {
+                  IFS=$'\n'
+                  for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                      delete_subvolume_recursively "/btrfs_tmp/$i"
+                  done
+                  btrfs subvolume delete "$1"
+              }
+
+              for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+                  delete_subvolume_recursively "$i"
+              done
+
+              btrfs subvolume create /btrfs_tmp/root
+              umount /btrfs_tmp
+            '';
+          };
         };
       };
     };
@@ -138,43 +173,6 @@
     shells = with pkgs; [zsh];
     variables = {
       EDITOR = "${pkgs.neovim}";
-    };
-  };
-
-  users = {
-    mutableUsers = false;
-    users = {
-      cody = {
-        isNormalUser = true;
-        shell = pkgs.zsh;
-        ignoreShellProgramCheck = true;
-        # hashedPassword = "$y$j9T$lIQkLrh/jE5pTGqAVcGCB/$YF1Uk6rTBsRednb2cr.ed8NP7.wKSWcYObe9Gl8RxN0";
-        hashedPasswordFile = config.sops.secrets.password.path;
-        # hashedPassword = "$y$j9T$Z2EZsUyOdN.ciPu/hYdlF.$4Jmti/Hm39QvSkP3rkht9GoSLhx3vI2Ouft1bnjyBo1";
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0jIg6UYuO+MSjBEcaaJXAoY3yLl7q7tqMVB0yFiqGr"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3fdO9C72OaYhK3W2OhINnlRCcopOblJJI/z9frc1F0"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwaOrqTJ6Xq8qU3y/Vn02tHMUZJISNRA/fLAVfYCN21"
-        ];
-        group = "cody";
-        extraGroups = [
-          "wheel"
-          "docker"
-          "wireshark"
-        ];
-      };
-      root = {
-        initialHashedPassword = "$y$j9T$vVNMHVnnesyBcnbDxNQ7T/$JqywYFyoePyzWLEW822Sv2pWYyQoLJgAo.wCzurMyNC";
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN0jIg6UYuO+MSjBEcaaJXAoY3yLl7q7tqMVB0yFiqGr"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3fdO9C72OaYhK3W2OhINnlRCcopOblJJI/z9frc1F0"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwaOrqTJ6Xq8qU3y/Vn02tHMUZJISNRA/fLAVfYCN21"
-        ];
-      };
-    };
-    # TODO:
-    groups = {
-      cody = {};
     };
   };
 
