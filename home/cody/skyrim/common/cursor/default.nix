1c7cb9de7ddc3d8ead58279776c203eb2a41fe25
Kodlak15 <stanlcod15@protonmail.com>
2024-10-09 14:24:42 -0700

    Udpate cursor (vscode) config

--- home/cody/skyrim/common/cursor/default.nix
+++ home/cody/skyrim/common/cursor/default.nix
@@ -1,8 +1,12 @@
 {pkgs, ...}: {
   home.packages = [pkgs.cursor];
 
+  # NOTE:
+  # No way to configure cursor with nix as of now
+  # Can load vscode config within cursor instead
+  # In order to do so vscode.enable = true must be set
   programs.vscode = {
-    enable = false;
+    enable = true;
     # package = pkgs.cursor;
     extensions = with pkgs.vscode-extensions; [
       # Color schemes
@@ -10,5 +14,55 @@
       # Editor modifications
       vscodevim.vim
     ];
+    keybindings = [];
+    userSettings = {
+      "vim.leader" = "<space>";
+      "vim.useSystemClipboard" = true;
+      "vim.handleKeys" = {
+        "<C-d>" = true;
+        "<C-f>" = true;
+      };
+      "workbench.colorTheme" = "Rosé Pine";
+      "vim.insertModeKeyBindings" = [
+        {
+          before = ["j" "k"];
+          after = ["<Esc>"];
+        }
+        {
+          before = ["k" "j"];
+          after = ["<Esc>"];
+        }
+      ];
+      "vim.normalModeKeyBindingsNonRecursive" = [
+        {
+          before = ["K"];
+          commands = ["editor.action.showDefinitionPreviewHover"];
+        }
+        {
+          before = ["g" "i"];
+          commands = ["editor.action.goToImplementation"];
+        }
+        {
+          before = ["g" "r"];
+          commands = ["editor.action.referenceSearch.trigger"];
+        }
+        {
+          before = ["g" "t"];
+          commands = ["editor.action.goToTypeDefinition"];
+        }
+        {
+          before = ["<leader>" "b" "d"];
+          commands = ["workbench.action.closeActiveEditor"];
+        }
+        {
+          before = ["H"];
+          commands = ["workbench.action.previousEditor"];
+        }
+        {
+          before = ["L"];
+          commands = ["workbench.action.nextEditor"];
+        }
+      ];
+    };
   };
 }
