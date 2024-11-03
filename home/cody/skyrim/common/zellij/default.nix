03fa940f3fec32599e687e9e833dfe99822f5f88
Kodlak15 <stanlcod15@protonmail.com>
2024-11-03 10:56:16 -0800

    Add README.md

--- home/cody/skyrim/common/zellij/default.nix
+++ home/cody/skyrim/common/zellij/default.nix
@@ -6,20 +6,13 @@
       tab_bar = false;
       default_layout = "compact";
       theme = "rose-pine";
+      keybinds = {
+        unbind = [
+          "Ctrl p"
+          "Ctrl n"
+        ];
+      };
       themes = {
-        "nord" = {
-          fg = "#D8DEE9";
-          bg = "#2E3440";
-          black = "#3B4252";
-          red = "#BF616A";
-          green = "#A3BE8C";
-          yellow = "#EBCB8B";
-          blue = "#81A1C1";
-          magenta = "#B48EAD";
-          cyan = "#88C0D0";
-          white = "#E5E9F0";
-          orange = "#D08770";
-        };
         "rose-pine" = {
           bg = "#403d52";
           fg = "#e0def4";
@@ -30,7 +23,6 @@
           magenta = "#c4a7e7";
           orange = "#fe640b";
           cyan = "#ebbcba";
-          # black = "#26233a";
           black = "#000000";
           white = "#e0def4";
         };
