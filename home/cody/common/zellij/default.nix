d0797bde318a329ef1810d56ed1efe53a00a1a3a
Kodlak15 <stanlcod15@protonmail.com>
2025-04-30 16:31:29 -0700

    Simplify zellij appearance

--- home/cody/common/zellij/default.nix
+++ home/cody/common/zellij/default.nix
@@ -6,28 +6,21 @@
       pane_frames = false;
       tab_bar = false;
       default_layout = "compact";
-      theme = "rose-pine";
+      theme = "ayu_dark";
       keybinds = {
         unbind = [
           "Ctrl p"
           "Ctrl n"
         ];
       };
-      themes = {
-        "rose-pine" = {
-          bg = "#403d52";
-          fg = "#e0def4";
-          red = "#eb6f92";
-          green = "#31748f";
-          blue = "#9ccfd8";
-          yellow = "#f6c177";
-          magenta = "#c4a7e7";
-          orange = "#fe640b";
-          cyan = "#ebbcba";
-          black = "#000000";
-          white = "#e0def4";
+      default_shell = "zsh";
+      ui = {
+        pane_frames = {
+          hide_session_name = true;
         };
       };
+      show_startup_tips = false;
+      show_release_notes = false;
     };
   };
 }
