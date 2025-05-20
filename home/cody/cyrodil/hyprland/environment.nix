c4a47e6d2f68498fccb02e07c1dfa9bcebf528c1
Kodlak15 <stanlcod15@protonmail.com>
2025-05-19 22:15:42 -0700

    De-nixify cyrodil hyprland env file

--- home/cody/cyrodil/hyprland/environment.nix
+++ home/cody/cyrodil/hyprland/environment.nix
@@ -1,64 +1,34 @@
 {
   wayland.windowManager.hyprland = {
-    settings.env = {
+    extraConfig = ''
       # Toolkit-specific scale
-      GDK_SCALE = 2;
+      env = GDK_SCALE,2
 
-      HYPRCURSOR_THEME = "rose-pine-hyprcursor";
-      HYPRCURSOR_SIZE = 16;
+      env = HYPRCURSOR_THEME,rose-pine-hyprcursor
+      env = HYPRCURSOR_SIZE,16
 
-      CLUTTER_BACKEND = "wayland";
-      SDL_VIDEODRIVER = "wayland";
-      XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-      MOZ_ENABLE_WAYLAND = "1";
-      MOZ_DBUS_REMOTE = "1";
-      GDK_BACKEND = "wayland,xll,*";
+      env = CLUTTER_BACKEND,wayland
+      env = SDL_VIDEODRIVER,wayland
+      env = XDG_SESSION_TYPE,wayland
+      env = XDG_CURRENT_DESKTOP,Hyprland
+      env = XDG_SESSION_DESKTOP,Hyprland
+      env = MOZ_ENABLE_WAYLAND,1
+      env = MOZ_DBUS_REMOTE,1
+      env = GDK_BACKEND,wayland,xll,*
 
-      QT_QPA_PLATFORM = "wayland;xcb";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      env = QT_QPA_PLATFORM,wayland;xcb
+      env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
 
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      LIBVA_DRIVER_NAME = "nvidia";
-      __GL_GSYNC_ALLOWED = "1";
+      env = GBM_BACKEND,nvidia-drm
+      env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+      env = LIBVA_DRIVER_NAME,nvidia
+      env = __GL_GSYNC_ALLOWED,1
 
-      NIXOS_OZONE_WL = "1";
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      env = NIXOS_OZONE_WL,1
+      env = ELECTRON_OZONE_PLATFORM_HINT,auto
 
       # Needed to run steam in offload mode
-      XDG_DATA_HOME = "$HOME/.local/share";
-    };
-    # extraConfig = ''
-    #   # Toolkit-specific scale
-    #   env = GDK_SCALE,2
-    #
-    #   env = HYPRCURSOR_THEME,rose-pine-hyprcursor
-    #   env = HYPRCURSOR_SIZE,16
-    #
-    #   env = CLUTTER_BACKEND,wayland
-    #   env = SDL_VIDEODRIVER,wayland
-    #   env = XDG_SESSION_TYPE,wayland
-    #   env = XDG_CURRENT_DESKTOP,Hyprland
-    #   env = XDG_SESSION_DESKTOP,Hyprland
-    #   env = MOZ_ENABLE_WAYLAND,1
-    #   env = MOZ_DBUS_REMOTE,1
-    #   env = GDK_BACKEND,wayland,xll,*
-    #
-    #   env = QT_QPA_PLATFORM,wayland;xcb
-    #   env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-    #
-    #   env = GBM_BACKEND,nvidia-drm
-    #   env = __GLX_VENDOR_LIBRARY_NAME,nvidia
-    #   env = LIBVA_DRIVER_NAME,nvidia
-    #   env = __GL_GSYNC_ALLOWED,1
-    #
-    #   env = NIXOS_OZONE_WL,1
-    #   env = ELECTRON_OZONE_PLATFORM_HINT,auto
-    #
-    #   # Needed to run steam in offload mode
-    #   env = XDG_DATA_HOME,$HOME/.local/share
-    # '';
+      env = XDG_DATA_HOME,$HOME/.local/share
+    '';
   };
 }
