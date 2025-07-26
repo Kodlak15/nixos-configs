6cdacc900a98b1d52a5fe6cb574164fb5c6163f1
Kodlak15 <stanlcod15@protonmail.com>
2025-07-25 21:39:20 -0700

    Update godot colors

--- home/cody/common/godot/default.nix
+++ home/cody/common/godot/default.nix
@@ -8,18 +8,32 @@
     settings = {
       interface.theme = {
         preset = "Custom";
+        # Ayu Dark
         base_color = {
-          red = 34.0;
-          green = 41.0;
-          blue = 54.0;
+          red = 13.0;
+          green = 16.0;
+          blue = 23.0;
           alpha = 1.0;
         };
         accent_color = {
-          red = 255.0;
-          green = 204.0;
-          blue = 102.0;
+          red = 230.0;
+          green = 180.0;
+          blue = 80.0;
           alpha = 1.0;
         };
+        # Ayu Mirage
+        # base_color = {
+        #   red = 34.0;
+        #   green = 41.0;
+        #   blue = 54.0;
+        #   alpha = 1.0;
+        # };
+        # accent_color = {
+        #   red = 255.0;
+        #   green = 204.0;
+        #   blue = 102.0;
+        #   alpha = 1.0;
+        # };
       };
       run.platforms.linuxbsd.preferWayland = true;
     };
