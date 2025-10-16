6f506a1e793b269cdff6fda9880c7cabe436fa86
Kodlak15 <stanlcod15@protonmail.com>
2025-10-15 20:53:31 -0700

    Add plymouth

--- hosts/cyrodil/default.nix
+++ hosts/cyrodil/default.nix
@@ -195,6 +195,26 @@
         splashImage = ./background.jpg;
       };
     };
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+      ];
+    };
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
     initrd = {
       postDeviceCommands = lib.mkAfter ''
         mkdir /btrfs_tmp
