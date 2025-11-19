7f11ed3c216a393f67a9cba183a12772fc57fcd9
Kodlak15 <stanlcod15@protonmail.com>
2025-11-18 17:30:40 -0800

    Remove 'v2' postfix from windowrule

--- home/cody/cyrodil/hyprland/windowRules.nix
+++ home/cody/cyrodil/hyprland/windowRules.nix
@@ -1,6 +1,6 @@
 {
   wayland.windowManager.hyprland = {
-    settings.windowrulev2 = [
+    settings.windowrule = [
       # ISSUE: https://github.com/hyprwm/Hyprland/discussions/8078
       "float, class:(brave)(.*), title:(Bitwarden)"
     ];
