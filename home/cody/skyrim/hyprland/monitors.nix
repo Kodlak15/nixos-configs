80d7c34b84bdd4e4121e73119f289ab8de8fc89f
Kodlak15 <stanlcod15@protonmail.com>
2026-01-11 15:21:54 -0800

    Update skyrim monitor scaling again

--- home/cody/skyrim/hyprland/monitors.nix
+++ home/cody/skyrim/hyprland/monitors.nix
@@ -1,7 +1,8 @@
 {
   wayland.windowManager.hyprland = {
     settings.monitor = [
-      "eDP-1, highrr, 0x0,    1.8"
+      # "eDP-1, highrr, 0x0,    1.8"
+      "eDP-1, highrr, 0x0,    1.6"
       " DP-1, highrr, 3456x0, 1"
     ];
   };
