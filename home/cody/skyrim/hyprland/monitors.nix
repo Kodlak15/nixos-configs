ebcc11525b6cb7334675be2c3db76dee3a51b0ce
Kodlak15 <stanlcod15@protonmail.com>
2026-01-11 15:09:17 -0800

    Fix borked install and change monitor scaling on laptop

--- home/cody/skyrim/hyprland/monitors.nix
+++ home/cody/skyrim/hyprland/monitors.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland = {
     settings.monitor = [
-      "eDP-1, highrr, 0x0,    1.5"
+      "eDP-1, highrr, 0x0,    1.8"
       " DP-1, highrr, 3456x0, 1"
     ];
   };
