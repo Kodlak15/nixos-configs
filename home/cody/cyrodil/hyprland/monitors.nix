622881de788d356c3daea72d4fe4784016a1759d
Kodlak15 <stanlcod15@protonmail.com>
2026-05-02 13:01:38 -0700

    Change desktop monitor order

--- home/cody/cyrodil/hyprland/monitors.nix
+++ home/cody/cyrodil/hyprland/monitors.nix
@@ -2,8 +2,8 @@
   wayland.windowManager.hyprland = {
     settings.monitor = [
       "DP-4, highrr, 0x0,    1"
-      "DP-5, highrr, 1920x0, 1"
-      "DP-3, highrr, 4480x0, 1"
+      "DP-3, highrr, 1920x0, 1"
+      "DP-5, highrr, 4480x0, 1"
     ];
   };
 }
